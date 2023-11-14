9bbf174fe2beda9789f10a1fa4d88f11fc60b60c
cinderblock-wall <142337901+cinderblock-wall@users.noreply.github.com>
2023-11-14 10:19:37 -0500

    Part 1

--- script.js
+++ script.js
@@ -8,24 +8,27 @@ const n4 = 5;
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-
+console.log (`sum of four numbers is 50: ${isSum50}.`)
 
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log (`At least two of the numbers are odd: ${isTwoOdd}.`)
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25; 
+console.log (`No number is over 25: ${isOver25}.`)
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -34,7 +37,7 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log (`The four numbers are valid according to the provided criteria: ${isValid}.`)
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
@@ -44,4 +47,24 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+console.log(n1 % 5===0)
+console.log(n2 % 5===0)
+console.log(n3 % 5===0)
+console.log(n4 % 5===0)
+
+//==========================
+
+console.log(n1 > n2)
+console.log(n2 > n3)
+console.log(n3 > n4)
+
+//==========================
+
+console.log(((n2 - n1)*n3)%n4)
+ 
+//==========================
+
+const isUnder25 = n1 <= 25 || n2 <= 25 || n3 < 25 || n4 < 25;
+console.log(isUnder25)
 
